5a92e1e2ae25e3350eaedc6d6fb21726933d254d
shrinidhihr <shrinidhi@thoughtclan.com>
2021-08-12 17:52:34 +0530

    Updated ideavimrc, and update script

--- update_files.sh
+++ update_files.sh
@@ -3,8 +3,7 @@
 # This file will copy settings from wsl into this folder.
 cp ~/.vimrc ./.vimrc_wsl
 
-# These are hard links. WARN: file paths may change later.
-# ln ./mnt/c/Users/hp/Documents/autoHotKey\ scripts/load_ahk.ahk ./ahkScripts/load_ahk.ahk
-# ln /mnt/c/Users/hp/.vimrc ./.vimrc_vscode
-# ln /mnt/c/Users/hp/.ideavimrc ./.ideavimrc
-
+cp /mnt/c/Users/hp/Documents/autoHotKey\ scripts/load_ahk.ahk ./ahkScripts/load_ahk.ahk
+cp /mnt/c/Users/hp/.vimrc ./.vimrc_vscode
+cp /mnt/c/Users/hp/.ideavimrc ./.ideavimrc
+echo "Done."
