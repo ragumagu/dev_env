3e379f96212787830f85de83f20346f5762ae77f
shrinidhihr <shrinidhi@thoughtclan.com>
2021-08-12 13:07:11 +0530

    Added ln commands

--- update_files.sh
+++ update_files.sh
@@ -3,3 +3,8 @@
 # This file will copy settings from wsl into this folder.
 cp ~/.vimrc ./.vimrc_wsl
 
+# These are hard links. WARN: file paths may change later.
+# ln ./mnt/c/Users/hp/Documents/autoHotKey\ scripts/load_ahk.ahk ./ahkScripts/load_ahk.ahk
+# ln /mnt/c/Users/hp/.vimrc ./.vimrc_vscode
+# ln /mnt/c/Users/hp/.ideavimrc ./.ideavimrc
+
