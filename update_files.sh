3ed8cf7944fcee74e51a5ea692d110670b503843
Shrinidhi H R <shrinidhi@thoughtclan.com>
2024-02-27 15:07:05 +0530

    Removed unused files

--- update_files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This file will copy settings from wsl into this folder.
-cp ~/.vimrc ./.vimrc_wsl
-
-cp /mnt/c/Users/hp/Documents/autoHotKey\ scripts/load_ahk.ahk ./ahkScripts/load_ahk.ahk
-cp /mnt/c/Users/hp/.vimrc ./.vimrc_vscode
-cp /mnt/c/Users/hp/.ideavimrc ./.ideavimrc
-echo "Done."
